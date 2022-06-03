337ab268e289037e6344ede841edb0f7dc2dcf12
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-03 15:02:05 +0100

    Refactor book class to use ES6 class syntax

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
 let myLibrary;
 
-function Book(title, author, pageCount, read) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pageCount, read) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
+  }
 
-Book.prototype.toTableCells = function() {
-  return `<td>${this.title}</td>
+  toTableCells() {
+    return `<td>${this.title}</td>
     <td>${this.author}</td>
     <td>${this.pageCount}</td>
     <td>${this.read}</td>`;
-}
+  }
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 if (localStorage.getItem('library') === null) {
   // demo book
-  myBook = new Book("How to Win Friends and Influence People", "Dale Carnegie", 268, false);
+  const myBook = new Book(
+    'How to Win Friends and Influence People',
+    'Dale Carnegie',
+    268,
+    false
+  );
   myLibrary = [myBook];
 } else {
   myLibrary = JSON.parse(localStorage.getItem('library'));
   // after deserialization the prototype needs to be set up manually
   myLibrary.forEach(book => {
     Object.setPrototypeOf(book, Book.prototype);
-  })
+  });
 }
 
 const form = document.querySelector('#form');
 const tableBody = document.querySelector('#table tbody');
-const showFormButton = document.querySelector("#show-form-btn");
+const showFormButton = document.querySelector('#show-form-btn');
 
 function addBookToLibrary(e) {
   e.preventDefault();
 
-  myLibrary.push(new Book(
-    form.title.value, form.author.value, form['page-count'].value,
-    form.read.checked ? true : false
-  ));
+  myLibrary.push(
+    new Book(
+      form.title.value,
+      form.author.value,
+      form['page-count'].value,
+      form.read.checked ? true : false
+    )
+  );
 
   form.reset();
   displayBooks(myLibrary);
@@ -56,7 +67,7 @@ function displayBooks(library) {
       displayBooks(library);
     });
 
-    readButton.textContent = book.read ? "Mark as not read" : "Mark as read";
+    readButton.textContent = book.read ? 'Mark as not read' : 'Mark as read';
     readButton.className = 'btn btn-secondary';
 
     readCell.appendChild(readButton);
@@ -72,14 +83,14 @@ function displayBooks(library) {
       displayBooks(library);
     });
 
-    deleteButton.textContent = "Delete";
+    deleteButton.textContent = 'Delete';
     deleteButton.className = 'btn btn-danger';
 
     deleteCell.appendChild(deleteButton);
     row.appendChild(deleteCell);
   }
 
-  tableBody.textContent = "";
+  tableBody.textContent = '';
 
   library.forEach((book, index) => {
     const row = document.createElement('tr');
@@ -98,13 +109,14 @@ displayBooks(myLibrary);
 
 form.addEventListener('submit', addBookToLibrary);
 
-
 showFormButton.addEventListener('click', () => {
   form.classList.toggle('hidden');
 
-  showFormButton.textContent =
-    form.classList.contains('hidden') ? 'Add new book' : 'Close form';
-  showFormButton.className =
-    form.classList.contains('hidden') ? 'btn btn-primary' : 'btn btn-danger';
+  showFormButton.textContent = form.classList.contains('hidden')
+    ? 'Add new book'
+    : 'Close form';
+  showFormButton.className = form.classList.contains('hidden')
+    ? 'btn btn-primary'
+    : 'btn btn-danger';
   showFormButton.classList.add('center');
 });
